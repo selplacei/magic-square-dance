cfc76ddc1003814e41a4d6fdd04cff9d4ce3052e
selplacei <35859522+selplacei@users.noreply.github.com>
2020-12-25 09:39:29 -0800

    Remove more redundancy and implement board initialization

--- board/board.py
+++ board/board.py
@@ -1,3 +1,4 @@
+from math import ceil, floor
 from typing import Dict, List, NewType, Optional, Tuple
 
 
@@ -15,15 +16,10 @@ YELLOW = SquareColor(2)
 GREEN = SquareColor(3)
 BLUE = SquareColor(4)
 
-ARROW_UP = ArrowDirection(0)
-ARROW_RIGHT = ArrowDirection(1)
-ARROW_DOWN = ArrowDirection(2)
-ARROW_LEFT = ArrowDirection(3)
-
 
 class Board:
     """
-    Data representation of an Aztec Board.
+    Data representation of an Aztec Diamond board.
 
     The board, stored in the `data` field, is as a list of dicts, where each dict represents two rows and each element
     of a dict is the X coordinate of a valid black square mapped to a tuple of the square's color and arrow direction.
@@ -38,11 +34,16 @@ class Board:
     If a valid square is not part of a domino, its color is GRAY.
     If the domino is vertical and the top square is black, its color is YELLOW; otherwise, it's RED.
     If the domino is horizontal and the left square is black, its color is BLUE; otherwise, it's GREEN.
-
-    Every valid non-gray square has an arrow direction, which indicates where the domino moves for the next board.
     """
-    def __init__(self, width):
-        self.data: List[Dict[int, Tuple[SquareColor, Optional[ArrowDirection]]]] = []
+    def __init__(self, height, init_data=True):
+        self.data: List[Dict[int, SquareColor]] = []
+        if init_data:
+            b = (height - 1) / 2
+            for i in range(height):
+                self.data.append({})
+                d = floor(-abs(i - b) + b)
+                for j in range(floor(b) - d, ceil(b) + d + 1):
+                    self.data[i][j] = GRAY
 
     @staticmethod
     def get_square_parity(x, y) -> SquareParity:
@@ -52,10 +53,10 @@ class Board:
         if not (0 <= y // 2 < len(self.data) and x in self.data[y // 2]):
             return NO_COLOR
         elif self.get_square_parity(x, y) == BLACK:
-            return self.data[y // 2][x][0]
+            return self.data[y // 2][x]
         else:
             neighbor = self.get_square_neighbor(x, y)
-            return self.data[neighbor[1] // 2][neighbor[0]][0]
+            return self.data[neighbor[1] // 2][neighbor[0]]
 
     def get_square_neighbor(self, x, y) -> Optional[Tuple[int, int]]:
         """Returns None if the square is gray or invalid. Always returns valid coordinates otherwise."""
@@ -71,21 +72,11 @@ class Board:
             return x + (1 if parity == BLACK else -1), y
         return None
 
-    def get_arrow_direction(self, x, y) -> Optional[ArrowDirection]:
-        """Returns None if the square is gray or invalid."""
-        if self.get_square_color(x, y) in {NO_COLOR, GRAY}:
-            return None
-        elif self.get_square_parity(x, y) == BLACK:
-            return self.data[y // 2][x][1]
-        else:
-            neighbor = self.get_square_neighbor(x, y)
-            return self.data[neighbor[1] // 2][neighbor[0]][1]
-
     def fill_holes(self):
-        """Fills all 2x2 areas of gray squares with new dominoes and gives them arrow directions."""
+        """Fills all 2x2 areas of gray squares with new dominoes."""
         pass
 
     def next_board(self) -> 'Board':
         """Performs necessary movement and deletion of dominoes according to current data and returns a new
-        instance of Board. ill_holes() will not be called by this method."""
+        instance of Board. fill_holes() will not be called by this method."""
         pass
